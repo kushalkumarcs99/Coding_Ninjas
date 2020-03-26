9104de2b3a0d9051f2a8f91e87abf66e9aaf1950
kushalkumarcs99 <37450254+kushalkumarcs99@users.noreply.github.com>
2020-03-26 08:39:03 +0530

    Create Count BSTs.cpp

---
+++ Count BSTs.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define mod 1000000007
+using namespace std;
+//int catalan( int n);
+//int binomialCoeff(int n, int k);
+
+//function
+int countBST( int n)
+{
+     int dp[n + 1]; 
+	fill_n(dp, n + 1, 0); 
+
+	// Base case 
+	dp[0] = 1; 
+	dp[1] = 1; 
+
+	// fill the dp table in top-down approach. 
+	for (int i = 2; i <= n; i++) { 
+		for (int j = 1; j <= i; j++) { 
+
+			// n-i in right * i-1 in left 
+			dp[i] =(dp[i]%mod + (dp[i - j] * dp[j - 1])%mod)%mod; 
+		} 
+	} 
+
+	return dp[n];
+
+}
